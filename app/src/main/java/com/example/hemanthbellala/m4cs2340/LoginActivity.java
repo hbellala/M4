a35392a82e8610696238bb2d019e09369f94b9e8
hemanthbellala <hemanth1bellala@gmail.com>
2017-09-21 15:19:20 -0400

    hemanth removing his comment

--- app/src/main/java/com/example/hemanthbellala/m4cs2340/LoginActivity.java
+++ app/src/main/java/com/example/hemanthbellala/m4cs2340/LoginActivity.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-//Hemanth bella first commit
+
 /**
  * A login screen that offers login via email/password.
  */
