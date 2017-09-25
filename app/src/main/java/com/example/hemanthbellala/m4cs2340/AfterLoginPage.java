becee35f80d2da8779d74b804d22a366b8349b5b
hemanthbellala <hemanth1bellala@gmail.com>
2017-09-25 00:04:03 -0400

    Addin the classes for welcome screen and afterlogin scrren

---
+++ app/src/main/java/com/example/hemanthbellala/m4cs2340/AfterLoginPage.java
@@ -0,0 +1,8 @@
+package com.example.hemanthbellala.m4cs2340;
+
+/**
+ * Created by hemanthbellala on 9/25/17.
+ */
+
+public class AfterLoginPage {
+}
--- app/src/main/java/com/example/hemanthbellala/m4cs2340/WelcomeScreen.java
+++ app/src/main/java/com/example/hemanthbellala/m4cs2340/WelcomeScreen.java
@@ -4,5 +4,5 @@ package com.example.hemanthbellala.m4cs2340;
  * Created by hemanthbellala on 9/21/17.
  */
 
-public class MainActivity {
+public class WelcomeScreen {
 }
