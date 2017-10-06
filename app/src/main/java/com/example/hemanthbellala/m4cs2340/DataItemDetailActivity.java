befa9a20e9080319a3eb43c898d5e98a9c058531
mpreilly <mpreilly@gatech.edu>
2017-10-05 21:26:47 -0400

    Made detail view activity. just address isn't working

--- app/src/main/java/com/example/hemanthbellala/m4cs2340/DataItemDetailActivity.java
+++ app/src/main/java/com/example/hemanthbellala/m4cs2340/DataItemDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.hemanthbellala.m4cs2340;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DataItemDetailActivity extends AppCompatActivity {
     private SightingDataItem mItem;
@@ -14,6 +15,15 @@ public class DataItemDetailActivity extends AppCompatActivity {
         int index = getIntent().getIntExtra("index", 1);
         mItem = Model.getInstance().getItemById(index);
 
+        ((TextView) findViewById(R.id.detail_key)).setText("" + mItem.getKey());
+        ((TextView) findViewById(R.id.date)).setText(mItem.getDate());
+        ((TextView) findViewById(R.id.location_type)).setText(mItem.getLocationType());
+        ((TextView) findViewById(R.id.zip)).setText(mItem.getZip());
+        ((TextView) findViewById(R.id.detail_address)).setText(mItem.getAddress());
+        ((TextView) findViewById(R.id.city)).setText(mItem.getCity());
+        ((TextView) findViewById(R.id.borough)).setText(mItem.getBorough());
+        ((TextView) findViewById(R.id.latitude)).setText(mItem.getLatitude());
+        ((TextView) findViewById(R.id.longitude)).setText(mItem.getLongitude());
 
     }
 }
